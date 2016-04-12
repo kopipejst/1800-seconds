bd546b3803a2c488da7061d575e58ef5194cc69a
Ivan Lazarevic <devet.sest@gmail.com>
2016-04-12 16:55:54 -0700

    Initial Commit

---
+++ 1800 secondsTests/_800_secondsTests.swift
@@ -0,0 +1,36 @@
+//
+//  _800_secondsTests.swift
+//  1800 secondsTests
+//
+//  Created by Ivan Lazarevic on 4/12/16.
+//  Copyright © 2016 Ivan Lazarevic. All rights reserved.
+//
+
+import XCTest
+@testable import _800_seconds
+
+class _800_secondsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
